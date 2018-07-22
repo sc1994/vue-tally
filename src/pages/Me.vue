ebd678270279da480f0985324c2bd3308a032112
SunCheng <sunchengsuncheng@gmail.com>
2018-07-22 13:39:03 +0800

    loginout

--- src/pages/Me.vue
+++ src/pages/Me.vue
@@ -1,7 +1,11 @@
 <template>
-    <main-layout>
-        wo de 
-    </main-layout>
+  <main-layout>
+    <mu-container class="button-wrapper">
+      <mu-flex justify-content="center" align-items="center">
+        <mu-button round color="success" full-width>退出登陆</mu-button>
+      </mu-flex>
+    </mu-container>
+  </main-layout>
 </template>
 
 <script>
@@ -10,6 +14,15 @@ import MainLayout from '../layouts/Main.vue'
 export default {
   components: {
     MainLayout
+  },
+  methods: {
+    loginOut() {
+      axios
+        .get('/removetoken/' + localStorage.getItem('token'))
+        .then(result => {
+          localStorage.clear()
+        })
+    }
   }
 }
 </script>
